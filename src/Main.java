b1d46a61830431f98be58c8e973036a96644ba36
g21turbo <dg21stew@gmail.com>
2023-11-19 17:43:34 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void getFirstNumber() {
+
+    }
+
+    public static void getSecondNumber() {
+
+    }
+
+    public static void setFirstNumber(double setFirstNumber) {
+
+    }
+
+    public static void setSecondNumber(double setSecondNumber) {
+
+    }
+
+
+}
